fcbdda0395f965a6d9c6b40c4bfacc2c8601f6cc
johnmartinroque <johnmartinroque@gmail.com>
2025-08-22 21:15:30 +0800

    roque commit

--- main/src/App.js
+++ main/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="text-center p-6 bg-red-100 min-h-screen">
       <h1 className="text-4xl font-bold text-blue-800">WEA RE SOOOOOO BACK</h1>
+      <h1>ROQUE TEST COMMIT</h1>
     </div>
   );
 }
