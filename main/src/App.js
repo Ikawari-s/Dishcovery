95686fda7ee2f0f63cf66c066a6509275e00c95a
Ikawari-s <lnmanese@student.hau.edu.ph>
2025-08-22 21:05:35 +0800

    first commit

---
+++ main/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center p-6 bg-red-100 min-h-screen">
+      <h1 className="text-4xl font-bold text-blue-800">WEA RE SOOOOOO BACK</h1>
+    </div>
+  );
+}
+
+export default App;
