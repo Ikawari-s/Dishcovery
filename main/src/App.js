c6bd3fc34efe494587937ea90c2136f7ebc6a6a8
johnmartinroque <johnmartinroque@gmail.com>
2025-08-22 21:50:25 +0800

    navbar/header

--- main/src/App.js
+++ main/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
 import Restaurants from "./screens/Restaurants";
 import Authentication from "./screens/Authentication";
+import Header from "./components/others/Header";
 
 function App() {
   return (
     <div className="text-center p-6 bg-red-100 min-h-screen">
       <Router>
+        <Header />
         <Routes>
           <Route element={<Home />} path="/" />
           <Route element={<Restaurants />} path="/restaurants" />
