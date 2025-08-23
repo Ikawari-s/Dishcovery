50d20e5b85445b9183eed438b5fdd7f395eaad8b
johnmartinroque <johnmartinroque@gmail.com>
2025-08-23 19:05:44 +0800

    removed map stuff

--- main/src/screens/Home.jsx
+++ main/src/screens/Home.jsx
@@ -1,25 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { fetchUsers } from "../services/userService";
-import Search from "../components/locations/Search";
+import React from "react";
 
 function Home() {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    fetchUsers().then((res) => setUsers(res.data));
-  }, []);
-  return (
-    <div>
-      <Search />
-      Home
-      <ul>
-        {users.map((u) => (
-          <li key={u.id}>
-            {u.name} ({u.email})
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
