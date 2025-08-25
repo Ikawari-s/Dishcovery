724b8624933253172e240ff23b344b46d9a27236
johnmartinroque <johnmartinroque@gmail.com>
2025-08-25 17:45:43 +0800

    working log in backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,29 @@
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // Find user by email
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -29,4 +49,4 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser };
+export { registerUser, authUser };
