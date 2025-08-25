4f86b51c5ef7e0d807342e931b31cb255b931e09
johnmartinroque <johnmartinroque@gmail.com>
2025-08-25 18:05:31 +0800

    working login frontend

--- main/src/components/Login.jsx
+++ main/src/components/Login.jsx
@@ -1,15 +1,38 @@
 import { useState } from "react";
+import { loginUser } from "../api/authenticationsApi";
 
 function Login() {
   const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
 
   const [password, setPassword] = useState("");
   const [showPass, setShowPass] = useState(false);
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessage("");
+
+    try {
+      setLoading(true);
+      const data = await loginUser({ email, password });
+
+      // Save token/user info in localStorage
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setMessage("Login successful!");
+
+      // Optional: redirect or update app state here
+      console.log("Logged in:", data);
+    } catch (error) {
+      setMessage(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div>
       Login
-      <form class="max-w-sm mx-auto">
+      <form class="max-w-sm mx-auto" onSubmit={handleSubmit}>
         <div class="mb-5">
           <label
             for="email"
@@ -63,23 +86,7 @@ function Login() {
             Show Password
           </label>
         </div>
-        <div class="flex items-start mb-5">
-          <div class="flex items-center h-5">
-            <input
-              id="remember"
-              type="checkbox"
-              value=""
-              class="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-              required
-            />
-          </div>
-          <label
-            for="remember"
-            class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >
-            Remember me
-          </label>
-        </div>
+
         <button
           type="submit"
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
