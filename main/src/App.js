906b0947ade3be408d835d937d1f7543e3b5bc86
Ikawari-s <lnmanese@student.hau.edu.ph>
2025-08-26 20:29:01 +0800

    loading screen

--- main/src/App.js
+++ main/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 import Home from "./screens/Home";
 import Restaurants from "./screens/Restaurants";
 import Authentication from "./screens/Authentication";
@@ -9,23 +11,36 @@ import ReviewDetailed from "./screens/ReviewDetailed";
 import Profile from "./screens/Profile";
 import User from "./screens/User";
 import Lists from "./screens/Lists";
+import LoadingScreen from "./components/others/LoadingScreen";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
   return (
-    <div className="text-center p-6 bg-red-100 dark:bg-gray-900 text-black dark:text-white min-h-screen">
-      <Router>
-        <Header />
-        <Routes>
-          <Route element={<Home />} path="/" />
-          <Route element={<Restaurants />} path="/restaurants" />
-          <Route element={<Authentication />} path="/authentication" />
-          <Route element={<ReviewDetailed />} path="/review/:id" />
-          <Route element={<RestaurantDetailed />} path="/restaurant/:id" />
-          <Route element={<User />} path="/user/:id" />
-          <Route element={<Profile />} path="/profile" />
-          <Route element={<Lists />} path="/lists" />
-        </Routes>
-      </Router>
+    <div className="relative text-center p-6 dark:bg-gray-900 text-black dark:text-white min-h-screen overflow-hidden">
+      {/* 👇 Loading screen covers the whole app, swipes up on finish */}
+      {loading && <LoadingScreen onFinish={() => setLoading(false)} />}
+
+      {/* 👇 Always render the app, but fade in when loading finishes */}
+      <div
+        className={`transition-opacity duration-500 ease-in-out ${
+          loading ? "opacity-0 pointer-events-none" : "opacity-100"
+        }`}
+      >
+        <Router>
+          <Header />
+          <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<Restaurants />} path="/restaurants" />
+            <Route element={<Authentication />} path="/authentication" />
+            <Route element={<ReviewDetailed />} path="/review/:id" />
+            <Route element={<RestaurantDetailed />} path="/restaurant/:id" />
+            <Route element={<User />} path="/user/:id" />
+            <Route element={<Profile />} path="/profile" />
+            <Route element={<Lists />} path="/lists" />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
