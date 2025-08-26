d773df01f2e4a12818fa09ec7586ee1feff59185
johnmartinroque <johnmartinroque@gmail.com>
2025-08-26 11:03:32 +0800

    restaurant and review models

---
+++ server/models/restaurantModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const restaurantSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    cuisine: {
+      type: String,
+      required: true,
+    },
+    address: {
+      street: { type: String, required: true },
+      city: { type: String, required: true },
+      zipcode: { type: String, required: true },
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    is_open: {
+      type: Boolean,
+      default: true,
+    },
+    tags: [String],
+    image: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+export default Restaurant;
