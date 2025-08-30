8bf33781069da2b2036153bf6a9d3a36761f3725
johnmartinroque <johnmartinroque@gmail.com>
2025-08-30 14:36:19 +0800

    feature branch

--- main/src/screens/Home.jsx
+++ main/src/screens/Home.jsx
@@ -4,27 +4,27 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-<div>
-  <div className="relative w-full h-[650px] overflow-hidden shadow-[0_60px_50px_20px_rgba(0,0,0,0.748)]">
-    <img
-      src={HomeFood}
-      alt="Delicious food"
-      className="w-[120%] h-full object-cover block"
-    />
+    <div>
+      <h1>Feature Branch Test</h1>
+      <div className="relative w-full h-[650px] overflow-hidden shadow-[0_60px_50px_20px_rgba(0,0,0,0.748)]">
+        <img
+          src={HomeFood}
+          alt="Delicious food"
+          className="w-[120%] h-full object-cover block"
+        />
 
-    <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#111827]/100 to-transparent" />
-  </div>
-  
-  <div className="mt-32">
-    <Link
-      to="/restaurants"
-      className="px-6 py-3 bg-yellow-500 text-white font-semibold rounded-full shadow-md hover:bg-yellow-600 transition duration-300"
-    >
-      Get Started
-    </Link>
-  </div>
-  
-</div>
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#111827]/100 to-transparent" />
+      </div>
+
+      <div className="mt-32">
+        <Link
+          to="/restaurants"
+          className="px-6 py-3 bg-yellow-500 text-white font-semibold rounded-full shadow-md hover:bg-yellow-600 transition duration-300"
+        >
+          Get Started
+        </Link>
+      </div>
+    </div>
   );
 }
 
