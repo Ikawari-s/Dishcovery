72ba7011a1256ca9bf47667a4658a433ab1b3052
Ikawari-s <lnmanese@student.hau.edu.ph>
2025-08-31 23:28:25 +0800

    what a drag
    haha get it haha

--- main/src/components/cards/RetaurantCard.jsx
+++ main/src/components/cards/RetaurantCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const RestaurantCard = ({ id, name, image, cuisine }) => {
   return (
-    <div className=" border border-gray-200 dark:border-gray-700 shadow-xl bg-yellow-50 dark:bg-gray-900 dark:border-gray-700 rounded-lg p-2 transition-all duration-300 hover:bg-yellow-100 hover:dark:bg-gray-800 hover:-translate-y-1">
+    <div className=" shadow-xl bg-yellow-50 dark:bg-gray-900 dark:border-gray-700 rounded-lg p-2 transition-all duration-300 hover:bg-yellow-100 hover:dark:bg-gray-800 hover:-translate-y-1">
       <Link to={`/restaurant/${id}`}>
         <img
           className="rounded-t-lg w-full h-48 object-cover"
