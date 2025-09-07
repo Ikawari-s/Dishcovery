ef0c7e0cf625d79f94948565dda202f28d20e24d
Ikawari-s <lnmanese@student.hau.edu.ph>
2025-09-07 17:52:44 +0800

    settings

--- main/src/screens/Settings.jsx
+++ main/src/screens/Settings.jsx
@@ -28,8 +28,9 @@ function TailwindTabs() {
   };
 
   return (
-    <div className="border-b border-gray-200 dark:border-gray-700">
-      <ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+    <div className="">
+      <ul className="flex flex-wrap justify-center -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+
         {/* Profile Tab */}
         <li className="me-2">
           <a
