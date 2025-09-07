53e7dbe723191f29be9dfead1bf8249be80eef10
johnmartinroque <johnmartinroque@gmail.com>
2025-09-07 22:28:44 +0800

    changed for to htmlFor and class to Classname in register and login

--- main/src/components/Login.jsx
+++ main/src/components/Login.jsx
@@ -118,11 +118,11 @@ function Login() {
           </div>
         </div>
       )}
-      <form class="max-w-sm mx-auto" onSubmit={handleSubmit}>
-        <div class="mb-5">
+      <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+        <div className="mb-5">
           <label
-            for="loginEmail"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="loginEmail"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Email
           </label>
@@ -131,42 +131,42 @@ function Login() {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             id="loginEmail"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="name@flowbite.com"
             required
           />
         </div>
 
-        <div class="mb-5">
+        <div className="mb-5">
           <label
-            for="loginPassword"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="loginPassword"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Password
           </label>
           <input
             type={showPass ? "text" : "password"}
             id="loginPassword"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <div class="flex items-start mb-5">
-          <div class="flex items-center h-5">
+        <div className="flex items-start mb-5">
+          <div className="flex items-center h-5">
             <input
               id="loginShowPass"
               type="checkbox"
               value=""
-              class="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+              className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
               checked={showPass}
               onChange={() => setShowPass(!showPass)}
             />
           </div>
           <label
-            for="showpass"
-            class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            htmlFor="showpass"
+            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
           >
             Show Password
           </label>
@@ -190,7 +190,7 @@ function Login() {
 
         <button
           type="submit"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           disabled={loading}
         >
           {loading ? "Logging in..." : "Submit"}
--- main/src/components/RegisterUser.jsx
+++ main/src/components/RegisterUser.jsx
@@ -119,11 +119,11 @@ function RegisterUser() {
           </div>
         </div>
       )}
-      <form class="max-w-sm mx-auto" onSubmit={handleSubmit}>
-        <div class="mb-5">
+      <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+        <div className="mb-5">
           <label
-            for="registerEmail"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="registerEmail"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Your email
           </label>
@@ -132,22 +132,22 @@ function RegisterUser() {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             id="registerEmail"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="name@flowbite.com"
             required
           />
         </div>
-        <div class="mb-5">
+        <div className="mb-5">
           <label
-            for="registerUsername"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="registerUsername"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Username
           </label>
           <input
             type="text"
             id="registerUsername"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="martinroque"
             required
             value={username}
@@ -155,33 +155,33 @@ function RegisterUser() {
           />
         </div>
 
-        <div class="mb-5">
+        <div className="mb-5">
           <label
-            for="registerPassword"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="registerPassword"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Password
           </label>
           <input
             type={showPass ? "text" : "password"}
             id="registerPassword"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <div class="mb-5">
+        <div className="mb-5">
           <label
-            for="registerPassword2"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            htmlFor="registerPassword2"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
             Confirm Password
           </label>
           <input
             type={showPass ? "text" : "password"}
             id="registerPassword2"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
             value={password2}
             onChange={(e) => setPassword2(e.target.value)}
@@ -202,27 +202,27 @@ function RegisterUser() {
           }}
         />
 
-        <div class="flex items-start mb-5">
-          <div class="flex items-center h-5">
+        <div className="flex items-start mb-5">
+          <div className="flex items-center h-5">
             <input
               id="registerShowPass"
               type="checkbox"
               value=""
-              class="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+              className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
               checked={showPass}
               onChange={() => setShowPass(!showPass)}
             />
           </div>
           <label
-            for="showpass"
-            class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            htmlFor="showpass"
+            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
           >
             Show Password
           </label>
         </div>
         <button
           type="submit"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           disabled={loading}
         >
           {loading ? "Creating Account" : "Create Account"}
