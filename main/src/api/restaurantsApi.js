307321e782e60e101e9a3275ca6c05f9c0c04c73
johnmartinroque <johnmartinroque@gmail.com>
2025-09-09 17:07:29 +0800

    restauratnts api fix

--- main/src/api/restaurantsApi.js
+++ main/src/api/restaurantsApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `http://localhost:5000/api/restaurants`;
+const API_URL = process.env.REACT_APP_RESTAURANTS_API_URL;
 
 export const getRestaurants = async () => {
   try {
