a92b8acfc3518c05c2271fbffefdb3105630b41f
Ikawari-s <lnmanese@student.hau.edu.ph>
2025-09-11 23:06:48 +0800

    spacing

--- main/src/screens/Settings.jsx
+++ main/src/screens/Settings.jsx
@@ -173,7 +173,7 @@ function TailwindTabs() {
       <div className="p-4">
         {activeTab === "profile" && (
           <div className="w-fit p-4 rounded-xl bg-white/40 dark:bg-gray-900/70 backdrop-blur-md shadow-xl mx-auto mt-4">
-            <h2 className="text-4xl font-bold mb-12 text-center">Profile Settings</h2>
+            <h2 className="text-4xl mt-4 font-bold mb-12 text-center">Profile Settings</h2>
             <div className="flex items-start gap-6">
               <Avatar />
               <ProfileSettings />
